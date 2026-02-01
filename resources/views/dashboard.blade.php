e05fe24496055447f36e23cd193ab16aab335806
Harshitha S <harshithaswamy3124@gmail.com>
2026-02-01 18:24:47 +0530

    ui changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,7 +12,7 @@
         } */
 
         body {
-            background: linear-gradient(135deg, #4da6ff, #28a745);
+            background: linear-gradient(69deg, #87a2bd, #82bf90);
             min-height: 100vh;
         }
 
@@ -125,10 +125,28 @@
 </head>
 <body>
 
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="navbar-brand" href="#">Shortner URL</a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <button id="logoutBtn">Logout &rarr;</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
 <div class="dashboard-container">
     <div class="dashboard-header">
         <h2 id="dashboardTitle">Dashboard</h2>
-        <button id="logoutBtn">Logout &rarr;</button>
     </div>
 
     <div class="button-group">
--- resources/views/invite.blade.php
+++ resources/views/invite.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
-            background: linear-gradient(135deg, #4da6ff, #28a745);
+            background: linear-gradient(69deg, #87a2bd, #82bf90);
             min-height: 100vh;
             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
         }
@@ -38,9 +38,26 @@
     </style>
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="navbar-brand" href="#">Shortner URL</a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <button id="logoutBtn" class="btn btn-danger">Logout &rarr;</button>
+      </form>
+    </div>
+  </div>
+</nav>
 
-<!-- Logout Button -->
-<button class="btn btn-danger logout-btn" id="logoutBtn">Logout &rarr;</button>
+
 
 <div class="container d-flex justify-content-center align-items-center vh-100">
     <div class="card">
